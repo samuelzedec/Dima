af165f52b1f3e133c80a4fa227eee05b29bf982d
Samuel Ribeiro <samuel.ribeiro77f@gmail.com>
2025-03-08 16:57:21 -0400

    Entendendo sobre Autenticação em blazor

--- Dima.Web/Program.cs
+++ Dima.Web/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Dima.Web;
+using Dima.Web.Security;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<CookieHandler>();
 
 builder.Services.AddMudServices();
 builder.Services.AddHttpClient(Configuration.HttpClientName, options =>
--- Dima.Web/Security/CookieAuthenticationStateProvider.cs
+++ Dima.Web/Security/CookieAuthenticationStateProvider.cs
@@ -1,3 +1,6 @@
+using System.Net.Http.Json;
+using System.Security.Claims;
+using Dima.Core.Models.Account;
 using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Dima.Web.Security;
@@ -5,20 +8,80 @@ namespace Dima.Web.Security;
 public class CookieAuthenticationStateProvider(IHttpClientFactory httpClientFactory)
     : AuthenticationStateProvider, ICookieAuthenticationStateProvider
 {
+    private const string IdentityPreffix = "v1/identity";
+    private bool _isAuthenticated = false;
     private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
-    
-    public Task<bool> CheckAuthenticatedAsync()
-    {
-        throw new NotImplementedException();
-    }
 
-    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    public async Task<bool> CheckAuthenticatedAsync()
     {
-        throw new NotImplementedException();
+        await GetAuthenticationStateAsync();
+        return _isAuthenticated;
     }
 
     public void NotifyAuthenticationStateChanged()
+        => base.NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
-        throw new NotImplementedException();
+        _isAuthenticated = false;
+        var user = new ClaimsPrincipal(new ClaimsIdentity());
+
+        var userInfo = await GetUser();
+        if (userInfo is null)
+            return new AuthenticationState(user);
+
+        var claims = await GetClaims(userInfo);
+        
+        var id = new ClaimsIdentity(claims, nameof(CookieAuthenticationStateProvider));
+        user = new ClaimsPrincipal(id);
+        _isAuthenticated = true;
+        return new AuthenticationState(user);
+    }
+
+    private async Task<User?> GetUser()
+    {
+        try
+        {
+            return await _client.GetFromJsonAsync<User>($"{IdentityPreffix}/manage/info");
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    private async Task<List<Claim>> GetClaims(User user)
+    {
+        List<Claim> claims =
+        [
+            new(ClaimTypes.Name, user.Email),
+            new(ClaimTypes.Email, user.Email)
+        ];
+
+        claims.AddRange(user.Claims
+            .Where(x => x.Key is not ClaimTypes.Name and ClaimTypes.Email)
+            .Select(x => new Claim(x.Key, x.Value)));
+
+        RoleClaim[]? roles;
+        try
+        {
+            roles = await _client.GetFromJsonAsync<RoleClaim[]>($"{IdentityPreffix}/roles");
+        }
+        catch (Exception e)
+        {
+            return claims;
+        }
+
+        foreach (var role in roles ?? [])
+        {
+            if (!string.IsNullOrEmpty(role.Type) && !string.IsNullOrEmpty(role.Value))
+                claims.Add(new Claim(role.Type, role.Value, role.ValueType, role.Issuer, role.OriginalIssuer));
+        }
+
+        claims.AddRange(roles
+            ?.Where(x => !string.IsNullOrEmpty(x.Type) && !string.IsNullOrEmpty(x.Value))
+            .Select(x => new Claim(x.Type!, x.Value!, x.ValueType, x.Issuer, x.OriginalIssuer)) ?? []);
+
+        return claims;
     }
 }
